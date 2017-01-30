229ee3e6466223b887033127d6155054da7fa82e
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-01-30 11:31:43 +0100

    Utworzenie interfejsu

---
+++ STS.Ikar.Interfaces/IDriversService.cs
@@ -0,0 +1,18 @@
+﻿using STS.Ikar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STS.Ikar.Interfaces
+{
+    public interface IDriversService
+    {
+        IList<Driver> Get();
+
+        void Add(Driver driver);
+
+        void Update(Driver driver);
+    }
+}
