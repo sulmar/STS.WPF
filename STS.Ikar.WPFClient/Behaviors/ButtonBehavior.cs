0e6e2c4b07258559657b28428e3cad9bd2e28579
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-01-31 14:08:33 +0100

    Utworzenie Behavior

---
+++ STS.Ikar.WPFClient/Behaviors/ButtonBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace STS.Ikar.WPFClient.Behaviors
+{
+    public class ButtonBehavior : Behavior<Button>
+    {
+        protected override void OnAttached()
+        {
+            this.AssociatedObject.MouseEnter += AssociatedObject_MouseEnter;
+
+             base.OnAttached();
+        }
+
+        private void AssociatedObject_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            var button = sender as Button;
+
+            button.Width =+100;
+
+        }
+    }
+}
