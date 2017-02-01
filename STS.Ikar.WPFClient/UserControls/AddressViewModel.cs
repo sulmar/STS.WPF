752a94140ca357fd1389adf9c0fc8d14bb938c82
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-02-01 13:46:24 +0100

    Utworzenie UserControl

---
+++ STS.Ikar.WPFClient/UserControls/AddressViewModel.cs
@@ -0,0 +1,21 @@
+﻿using STS.Ikar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STS.Ikar.WPFClient.UserControls
+{
+    public class AddressViewModel
+    {
+        public Address Address { get; set; }
+
+        public AddressViewModel()
+        {
+
+        }
+
+
+    }
+}
