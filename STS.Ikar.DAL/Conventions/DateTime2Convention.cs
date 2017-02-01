d7b612a633bb88504661b7ac78670997b3f9a3b5
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-02-01 11:39:45 +0100

    Utworzenie własnej konwencji

---
+++ STS.Ikar.DAL/Conventions/DateTime2Convention.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STS.Ikar.DAL.Conventions
+{
+    public class DateTime2Convention : Convention
+    {
+        public DateTime2Convention()
+        {
+            this.Properties<DateTime>()
+                 .Configure(c => c.HasColumnType("datetime2")
+                     .HasPrecision(0));
+        }
+    }
+}
