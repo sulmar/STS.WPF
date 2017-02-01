4b54a2900c3dea08170c52fd5711c164010beecb
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-02-01 12:58:08 +0100

    Walidacja

--- STS.Ikar.Models/Driver.cs
+++ STS.Ikar.Models/Driver.cs
@@ -1,6 +1,7 @@
 ﻿using PropertyChanged;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,11 +12,26 @@ namespace STS.Ikar.Models
     // PM> Install-Package PropertyChanged.Fody
 
     [ImplementPropertyChanged]
-    public class Driver : Base
+    public class Driver : Base, IDataErrorInfo
     {
         public int DriverId { get; set; }
 
-        public string FirstName { get; set; }
+        private string _FistName;
+
+        public string FirstName
+        {
+            get { return _FistName; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentNullException("Imię jest puste");
+
+                _FistName = value;
+
+            }
+        }
+
+
 
         public string LastName { get; set; }
 
@@ -28,6 +44,38 @@ namespace STS.Ikar.Models
         public int Weight { get; set; }
 
         public string FullName => $"{FirstName} {LastName}";
+
+        public string Error
+        {
+            get
+            {
+                return string.Empty;
+            }
+        }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                String errorMessage = String.Empty;
+                switch (columnName)
+                {
+                    case nameof(FirstName):
+                        if (String.IsNullOrEmpty(FirstName))
+                        {
+                            errorMessage = "First Name is required";
+                        }
+                        break;
+                    case nameof(LastName):
+                        if (String.IsNullOrEmpty(LastName))
+                        {
+                            errorMessage = "Nazwisko jest wymagane";
+                        }
+                        break;
+                }
+                return errorMessage;
+            }
+        }
 
         public override string ToString() => FullName;
     }
