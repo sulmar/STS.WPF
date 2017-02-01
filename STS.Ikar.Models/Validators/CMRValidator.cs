8e16ba91e009d3110fa97970ff1fdc355131ff00
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-02-01 13:29:45 +0100

    Integracja FluentValidation z WPF

---
+++ STS.Ikar.Models/Validators/CMRValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STS.Ikar.Models.Validators
+{
+    public class CMRValidator : AbstractValidator<CMR>
+    {
+        public CMRValidator()
+        {
+            RuleFor(p => p.WarehouseDocuments)
+                .Must(p => p.Any());
+        }
+    }
+}
---
+++ STS.Ikar.Models/Validators/DriverValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STS.Ikar.Models.Validators
+{
+    public class DriverValidator : AbstractValidator<Driver>
+    {
+        public DriverValidator()
+        {
+            RuleFor(p => p.FirstName)
+                .NotEmpty();
+
+            RuleFor(p => p.LastName)
+                .NotEmpty();
+
+            RuleFor(p => p.Birthday)
+                .GreaterThanOrEqualTo(DateTime.Today)
+                .WithMessage("Data urodzenia nie może być wcześniejsza niż dzień dzisiejszy");
+
+        }
+    }
+}
